89a074255982009a775ee6a85d6608dbbc44e058
LuísCosta <luistelmocosta@gmail.com>
2016-11-10 16:17:40 +0000

    app settings

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager, UserMixin, \
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
